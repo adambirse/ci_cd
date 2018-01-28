f1a5dba6a2350fec1d9852c2130c18d4fc715c60
Adam Birse <adambirse@gmail.com>
2018-01-28 21:33:13 +0000

    Initial commit

---
+++ src/main/java/com/birse/ci_cd/CiCdApplication.java
@@ -0,0 +1,12 @@
+package com.birse.ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdApplication.class, args);
+	}
+}
