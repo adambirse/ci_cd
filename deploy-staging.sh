ae8022da646d8116cbae1f439e4f3a5b434152ba
Adam Birse <abirse@worth.systems>
2018-11-16 09:14:11 +0000

    debugging added.

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -3,18 +3,29 @@
 set -e
 cd docker/build_context
 docker build -t gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT .
+echo "docker build successful"
 
 echo $GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+echo "Auth successful"
+
+echo $PROJECT_NAME_STG
+echo $CLUSTER_NAME_STG
+echo ${CLOUDSDK_COMPUTE_ZONE}
+echo $CLUSTER_NAME_STG
 
 gcloud --quiet config set project $PROJECT_NAME_STG
 gcloud --quiet config set container/cluster $CLUSTER_NAME_STG
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_STG
 
+echo "About to push to cloud"
 gcloud docker push gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}
 
+echo "push successful"
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}:latest
+
+echo "tag successful"
 
 kubectl config view
 kubectl config current-context
