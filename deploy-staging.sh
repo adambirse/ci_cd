9b0f3674b5f4f4d3d591da92b617cfcc9eb55583
Adam Birse <abirse@worth.systems>
2018-11-16 10:46:05 +0000

    moved docker build into main staging script.

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -7,6 +7,8 @@ echo $GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.j
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 echo "Auth successful"
 
+
+
 echo $PROJECT_NAME_STG
 echo $CLUSTER_NAME_STG
 echo ${CLOUDSDK_COMPUTE_ZONE}
@@ -17,8 +19,15 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME_STG
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_STG
 
+echo "About to build"
+cd $TRAVIS_BUILD_DIR/docker/build_context
+docker build -t gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT .
+cd $TRAVIS_BUILD_DIR
+
 echo "About to push to cloud"
+cd build/
 gcloud docker push gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}
+cd $TRAVIS_BUILD_DIR
 
 echo "push successful"
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}:latest
--- docker/prepare_build_docker.sh
+++ docker/prepare_build_docker.sh
@@ -2,6 +2,4 @@
 
 rm -rf build/ci_cd-0.0.1-SNAPSHOT.jar
 cp ../build/libs/ci_cd-0.0.1-SNAPSHOT.jar ./build_context
-cd build_context
-docker build -t "ci_cd" .
-cd ..
+
